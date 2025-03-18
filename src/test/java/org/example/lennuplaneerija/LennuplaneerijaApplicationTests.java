c0f735b687c0ee19eef793ca143752590ed24693
Alexandra Voit <alexandra.voit@ut.ee>
2025-03-18 16:16:22 +0200

    Projekti baasi loomine: backend SpringBootis + frontend Vues

---
+++ src/test/java/org/example/lennuplaneerija/LennuplaneerijaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lennuplaneerija;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LennuplaneerijaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
