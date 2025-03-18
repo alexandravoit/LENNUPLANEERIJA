a2506132969e7b7faaa617d03fc4b582e3dca18d
Alexandra Voit <alexandra.voit@ut.ee>
2025-03-18 16:26:45 +0200

    frontend failid

---
+++ src/frontend/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <p>{{message}}</p>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'App',
+
+  data() {
+    return {
+      message: ''
+    };
+  },
+
+  created() {
+    this.lennukid();
+  },
+  methods: {
+    async lennukid() {
+      try {
+        const response = await axios.get('/api/lennukid');
+        this.message = response.data;
+        console.log(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+  }
+
+}
+
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
