6bf359ea683e0ea7075e0692c05b5552367189a9
Alexandra Voit <alexandra.voit@ut.ee>
2025-03-22 13:20:01 +0200

    Frontend arendus: Landing lehe disain

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,32 +1,32 @@
 <template>
-  <p>{{message}}</p>
+  <div class="app">
+    <div class="header">
+      <h1 class="green">SWAG</h1>
+      <h1 class="white">TRAVEL</h1>
+    </div>
+    <h2 class="subheader">KOIGE SWAGIM LENNUPLANEERIJA</h2>
+
+    <img src="@/assets/plane.svg" class="plane"/>
+
+  </div>
+
+
 </template>
 
 <script>
-import axios from 'axios';
+// import axios from 'axios';
 
 export default {
   name: 'App',
 
   data() {
-    return {
-      message: ''
-    };
+
   },
 
   created() {
-    this.lennukid();
   },
   methods: {
-    async lennukid() {
-      try {
-        const response = await axios.get('/api/lennukid');
-        this.message = response.data;
-        console.log(response.data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    }
+
   }
 
 }
@@ -35,12 +35,64 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+@font-face {
+  font-family: 'Akira';
+  src: url('@/assets/fonts/Akira.otf') format('opentype');
 }
+
+* {
+  font-family: 'Akira', sans-serif;
+  background: black;
+  color: white;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+.app {
+  border: solid 1px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.header {
+  display: flex;
+  flex-direction: row;
+  font-size: 4vw;
+}
+
+.header .green {
+  color: #00FF92;
+}
+
+.header .white {
+  color: white;
+}
+
+.subheader {
+  font-size: 3vw;
+  color: black;
+  -webkit-text-stroke: 1px #00FF92;
+  margin-bottom: 1vh;
+}
+
+.plane {
+  width: 60vw;
+  animation: float 3s ease-in-out infinite;
+}
+
+
+
+@keyframes float {
+  0% { transform: translateY(0) }
+  50% { transform: translateY(-1vh) }
+  100% { transform: translateY(0) }
+}
+
+
+
+
+
 </style>
